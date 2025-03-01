913fed0b52708bbaed301d3e9021083544c2caad
Michael Gold <goldmichael@gmail.com>
2025-03-01 09:49:45 -0700

    fix: all origins

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://zyx.permute.xyz"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
